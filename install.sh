dd61ed3ee6171764d7d46266fe44c17e05b7b382
chmod644 <chmod644>
2016-09-25 20:30:43 +0900

    Add install file for dotfiles

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+backupdir=~/Backup/initial_dotfiles
+
+dotfiles=(           \
+    ".bashrc"        \
+    ".bashrc_common" \
+    ".gitconfig"     \
+    ".screenrc"      \
+    ".vim"           \
+    ".vimrc"         \
+)
+
+# Make backup directory
+if [ -e ${backupdir} ]; then
+    echo "Error: Directory ${backupdir} exists"
+    exit 1
+fi
+mkdir -p ${backupdir}
+
+# Make symbolic link of dotfiles
+for f in ${dotfiles[@]}; do
+    if [ -e ~/$f ]; then
+        mv ~/$f ${backupdir}
+    fi
+    realpath=`readlink -f $f`
+    ln -s ${realpath} ~/$f
+done
+
+# Install vim plugins
+vim +":call dein#install()" +:q
