e52bdaca3011259bf69dce108ea8be4038cffcb7
chmod644 <chmod644>
2016-09-25 20:54:15 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,5 +28,6 @@ for f in ${dotfiles[@]}; do
 done
 
 # Install vim plugins
-git clone https://github.com/Shougo/dein.vim ~/.vim/dein
-vim +":call dein#install()" +:q
+mkdir -p ~/.vim/dein/repos/github.com/Shougo/
+git clone https://github.com/Shougo/dein.vim ~/.vim/dein/repos/github.com/Shougo/dein.vim
+vim +":silent call dein#install()" +:q
